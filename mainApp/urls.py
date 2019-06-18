d1e5dcaa9d57e4d0fcb19368498781b7d32f706e
fosquito <39629614+fosquito@users.noreply.github.com>
2019-06-18 12:34:51 +0100

    Update urls.py

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
